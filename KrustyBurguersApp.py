daf380bc2d6c261fa3725fb3d16fcc60bcd026ff
Arthur Vinícius <108879248+ArthurVinxcius@users.noreply.github.com>
2025-02-07 22:29:25 -0300

    Update KrustyBurguersApp.py
    Interface de Login finalizada e correção de bugs gráficos

--- KrustyBurguersApp.py
+++ KrustyBurguersApp.py
@@ -187,23 +187,24 @@ class App(ctk.CTk, BackEnd):
         '''self.logomarca = ctk.CTkImage(light_image= Image.open("logomarca3.png"), dark_image = Image.open("logomarca3.png"), size = (250, 250))
         self.logomarca = ctk.CTkLabel(self.background, image=self.logomarca, text=None, width=100,bg_color="transparent", height=100).place(x=(600-350)/2, y=150)'''
 
-
         #login frame
         self.frame_login = ctk.CTkFrame(self, fg_color=["#FDFDFD","#0D0D0D"], width=350, height=600)
         self.frame_login.pack(side = "right")
 
-        #login frames widgets
-        self.login_label = ctk.CTkLabel(self.frame_login, text="Welcome to Krusty's App", text_color= "blue", font=("Arial", 20))
-        self.login_label.place(x=50, y=50)
-        
-        self.usuario_entry = ctk.CTkEntry(self.frame_login, placeholder_text_color=None, width=250, height= 35, font=("Arial", 14)) #tentar colocar width=1 com a imagem pra testar se da certo
-        self.usuario_entry.place(relx=0.5, y=241.3, anchor = "center")
+        self.usuario_imag = ctk.CTkImage(light_image=Image.open("username.png"), dark_image=Image.open("username.png"), size=(114, 27))
+        self.usuario_imag = ctk.CTkLabel(self.frame_login, image=self.usuario_imag, text = None).place(x= 38.07,y=216.19)
+
+        self.usuario_entry = ctk.CTkEntry(self.frame_login, placeholder_text_color=None, fg_color=["#FDFDFD","#0D0D0D"], border_width = 2, border_color="#9C00D4", corner_radius= 100, width=300, height= 25, font=("Sometype Mono Bold", 14)) #tentar colocar width=1 com a imagem pra testar se da certo
+        self.usuario_entry.place(relx=0.5, y=255, anchor = "center")
 
         '''self.usuario_label = ctk.CTkLabel(self.frame_login, text="*Campo obrigatório", height=0,text_color="blue", font=("Arial", 10))
         self.usuario_label.place(x=45, y=188)'''
 
-        self.senha_entry = ctk.CTkEntry(self.frame_login, placeholder_text="Senha", width=250, height= 35, font=("Arial", 14), show = "*")
-        self.senha_entry.place(relx = 0.5, y=314.86, anchor = "center")
+        self.senha_img = ctk.CTkImage(light_image=Image.open("password.png"), dark_image=Image.open("password.png"), size=(80, 26))
+        self.senha_img = ctk.CTkLabel(self.frame_login, image=self.senha_img, text=None).place(x=38.07, y=290.03)
+
+        self.senha_entry = ctk.CTkEntry(self.frame_login, placeholder_text_color=None, fg_color=["#FDFDFD","#0D0D0D"], border_width = 2, border_color="#9C00D4", corner_radius= 100, width=300, height= 25, font=("Sometype Mono Bold", 14), show = "*")
+        self.senha_entry.place(relx = 0.5, y=330, anchor = "center")
 
         '''self.senha_label = ctk.CTkLabel(self.frame_login, text="*Campo obrigatório", height=0,text_color="blue", font=("Arial", 10))
         self.senha_label.place(x=45, y=243)'''
@@ -211,7 +212,8 @@ class App(ctk.CTk, BackEnd):
         self.lembrar_check = ctk.CTkCheckBox(self.frame_login, checkbox_width=12.36, checkbox_height=12.36, border_color = "#9C00D4", border_width= 1.35, corner_radius= 2, checkmark_color="#9C00D4", text="Manter usuário", text_color= "#9C00D4", fg_color=["#FDFDFD","#0D0D0D"], hover = False, font=("Sometype Mono SemiBold", 12.26))
         self.lembrar_check.place(x=37.5, y=364.49)
 
-        self.login_button = ctk.CTkButton(self.frame_login, text="Login", width=1, height=1, font=("Arial", 14), command=self.verifica_login)
+        self.login_button = ctk.CTkImage(light_image=Image.open("BotãoDeLogin.png"), dark_image=Image.open("BotãoDeLogin.png"), size=(114, 34))
+        self.login_button = ctk.CTkButton(self.frame_login, image= self.login_button, text=None, bg_color=["#FDFDFD","#0D0D0D"], fg_color=["#FDFDFD","#0D0D0D"], hover= None, width=1, height=1, command=self.verifica_login)
         self.login_button.place(relx = 0.5, y= 452.86, anchor = "center")
 
         self.cadastro_button = ctk.CTkButton(self.frame_login, text="CADASTRE-SE", text_color= "#9C00D4", fg_color=["#FDFDFD","#0D0D0D"], width=1, height=1, hover = False, font=("Sometype Mono Bold", 10.21), command=self.cadastro)
@@ -221,6 +223,12 @@ class App(ctk.CTk, BackEnd):
         self.esqueceu_button.place(x=185.48, y= 364.49)
 
         self.creditos = ctk.CTkLabel(self.frame_login, text="Development by ArthurVinxcius and isjustjefferson", text_color= "#9C00D4", font=("Sometype Mono Bold", 10.21)).place(relx = 0.5, y= 578.37, anchor = "center")
+
+        self.login_title = ctk.CTkImage(light_image=Image.open("Title.png"), dark_image=Image.open("Title.png"), size=(206, 75))
+        self.login_title = ctk.CTkLabel(self.frame_login, image= self.login_title, text = None).place(x = 26.49, y = 18.63)
+
+        self.boneco_grande = ctk.CTkImage(light_image=Image.open("BonecoGrande.png"), dark_image=Image.open("BonecoGrande.png"),size=(70, 70))
+        self.boneco_grande = ctk.CTkLabel(self.frame_login, image=self.boneco_grande, text=None).place(relx = 0.5, y=158.34, anchor = "center")
 
     #tela de registro
     def cadastro(self):
